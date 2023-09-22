0e7b0a02f1586b320099a7c0c42cdb6e4bc5c3f8
arichild <arieevq@gmail.com>
2023-09-22 16:12:49 +0300

    adding circle profiles

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -162,6 +162,18 @@ const openPopup = async (id) => {
   parsePopup(data)
 }
 
+document.addEventListener('click', (e) => {
+  if(e.target.closest('.screen-main-person') || e.target.classList.contains('screen-main-person')) {
+    const parent = e.target.closest('.screen-main-person') || e.target
+
+    parent.querySelector('.screen-main-overlay').classList.toggle('active')
+  } else {
+    document.querySelectorAll('.screen-main-overlay').forEach(item => {
+      item.classList.remove('active')
+    })
+  }
+})
+
 //парсим результаты поиска
 const parseSearchResults = (arr, phrase) => {
   let output = ''
@@ -203,6 +215,7 @@ document.querySelector('.search-form-form')
     }
 
     const res = await fetchSearch(phrase)
+
     const parsedResults = parseSearchResults(res.profiles, phrase)
     document.querySelector('.search-results').innerHTML = parsedResults
     document.querySelector('.search-drop').classList.add('opened');
@@ -220,3 +233,150 @@ document.addEventListener('click',(e) => {
     document.querySelector('.search-drop').classList.remove('opened');
   }
 })
+
+let page = 1
+let lastPage
+let flag = true
+
+const fetchList = async (page) => {
+  try {
+    const response = await fetch(`//apk.farbatest.com/api/profile/get_page?page=${page}`)
+    const { status } = response;
+    const { profiles, last_page } = await response.json();
+
+    lastPage = last_page
+
+    return { status, profiles, last_page };
+  } catch (error) {
+    console.error(error);
+    return { status: 500, profiles: [] };
+  }
+};
+
+const parserProfiles = (arr) => {
+  let output = ''
+
+  if (!arr || !arr.length) {
+    output = `<div class="search-not-found">Нет результатов по запросу</div>`
+    return output
+  }
+
+  arr.forEach((el, index) => {
+    output +=
+    `
+      <div class="screen-main-person cirlce-${index+1}">
+        <div class="screen-main-overlay">
+          <div class="screen-main-name">${el.fio}</div>
+
+          <a href="javascript:void(0)" data-id="${el.id}" onclick="openPopup(${el.id})" class="screen-main-more">Узнать больше</a>
+        </div>
+
+        <img src="//apk.farbatest.com/storage/${el.photo_cropped}" alt="alt">
+      </div>
+    `
+  })
+
+  return output
+}
+
+const onLoad = async (page) => {
+  const { profiles } = await fetchList(page);
+  const parsedResults = parserProfiles(profiles);
+
+  document.querySelector('.screen-main-profiles').innerHTML = parsedResults;
+};
+
+window.addEventListener('load', () => {
+  onLoad(page)
+});
+
+const updateCards = async(page) => {
+  const allProfiles = document.querySelectorAll('.screen-main-person')
+
+  allProfiles.forEach(item => {
+    item.remove()
+  })
+
+  await onLoad(page)
+
+  flag = true
+}
+
+document.querySelector('.screen.screen-main').addEventListener('touchstart', handleTouchStart, false);
+document.querySelector('.screen.screen-main').addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches || // чистый API JS
+  evt.originalEvent.touches; // jQuery
+}
+
+function handleTouchStart(evt) {
+  const firstTouch = getTouches(evt)[0];
+  xDown = firstTouch.clientX;
+  yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+  if ( ! xDown || ! yDown ) {
+    return;
+  }
+
+  var xUp = evt.touches[0].clientX;
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+
+  const parent = document.querySelector('.screen-main-profiles')
+
+  if ( Math.abs( xDiff ) < Math.abs( yDiff ) ) {
+    if ( yDiff > 0 ) {
+      // swipe вверх
+      if(!flag) return
+
+      if(page < lastPage) {
+        flag = false
+        page = page + 1
+
+        parent.classList.add('active')
+
+        setTimeout(() => {
+          updateCards(page)
+        }, 600)
+
+        setTimeout(() => {
+          parent.classList.remove('active')
+        }, 1000)
+      }
+
+    } else {
+      /* swipe вниз */
+
+      if(!flag) return
+
+      if(page > 1) {
+        flag = false
+        page = page - 1
+
+        parent.classList.add('active')
+
+        setTimeout(() => {
+          updateCards(page)
+          console.log(1)
+        }, 600)
+
+        setTimeout(() => {
+          parent.classList.remove('active')
+          console.log(2)
+        }, 1000)
+      }
+    }
+  }
+
+  /* свайп был, обнуляем координаты */
+  xDown = null;
+  yDown = null;
+};
